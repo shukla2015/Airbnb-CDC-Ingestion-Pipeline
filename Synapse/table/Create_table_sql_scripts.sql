7112c8acb6b5ebf73215029725e8dd2831ed4172
shashank shukla <135333924+shukla2015@users.noreply.github.com>
2026-06-05 08:46:30 +0530

    Add files via upload

---
+++ Synapse/table/Create_table_sql_scripts.sql
@@ -0,0 +1,44 @@
+create schema airbnb;
+
+CREATE TABLE airbnb.customer_dim (
+    customer_id INT, 
+    first_name NVARCHAR(100),
+    last_name NVARCHAR(100),
+    email NVARCHAR(255),
+    phone_number NVARCHAR(50),
+    address NVARCHAR(255),
+    city NVARCHAR(100),
+    state NVARCHAR(100),
+    country NVARCHAR(100),
+    zip_code NVARCHAR(20),
+    signup_date DATE,
+    last_login DATETIME,
+    total_bookings INT,
+    total_spent DECIMAL(10, 2),
+    preferred_language NVARCHAR(50),
+    referral_code NVARCHAR(50),
+    account_status NVARCHAR(50)
+);
+
+select * from airbnb.customer_dim;
+
+-------------------------------------------------
+
+CREATE TABLE airbnb.bookings_fact (
+    booking_id NVARCHAR(100),
+    property_id NVARCHAR(100),
+    customer_id INT,
+    owner_id NVARCHAR(100),
+    check_in_date DATE,
+    check_out_date DATE,
+    booking_date DATETIME,
+    amount FLOAT,
+    currency NVARCHAR(10),
+    city NVARCHAR(100),
+    country NVARCHAR(100),
+    full_address NVARCHAR(255),
+    stay_duration BIGINT,
+    booking_year INT,
+    booking_month INT,
+    timestamp DATETIME
+);
