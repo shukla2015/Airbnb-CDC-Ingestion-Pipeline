9118717ac689beb8b571ebce4bb0098158ea1a68
shashank shukla <135333924+shukla2015@users.noreply.github.com>
2026-06-05 08:50:41 +0530

    Add files via upload

--- Synapse/table/Create_table_sql_scripts.sql
+++ Synapse/table/Create_table_sql_scripts.sql
@@ -42,3 +42,19 @@ CREATE TABLE airbnb.bookings_fact (
     booking_month INT,
     timestamp DATETIME
 );
+
+
+CREATE TABLE airbnb.BookingCustomerAggregation
+WITH (DISTRIBUTION = ROUND_ROBIN)
+AS
+SELECT 
+    c.country,
+    COUNT_BIG(*) AS total_bookings,
+    SUM(ISNULL(b.amount, 0)) AS total_amount,
+    MAX(b.booking_date) AS last_booking_date
+FROM 
+    airbnb.bookings_fact b
+JOIN 
+    airbnb.customer_dim c ON b.customer_id = c.customer_id
+GROUP BY 
+    c.country;
